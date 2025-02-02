8e1f47498fada336b2ae5afe89e9dae9e4dc49d8
rshaki01 <rshaki01@gmail.com>
2025-02-01 19:29:26 -0500

    Cleaned up homepage

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -9,7 +9,7 @@ const myFont = localFont({
 const Navbar = () => {
 
   return (
-    <div className='flex items-center justify-center h-20 bg-white w-full fixed top-0 left-0 z-50 border-b-2 border-black'>
+    <div className='flex items-center justify-center h-20 bg-white w-full sticky top-0 left-0 z-50 border-b-2 border-black'>
       <Link href='/'>
         <h1 className={`${myFont.className} text-black text-4xl cursor-pointer`}>Stripes!</h1>
       </Link>
--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,18 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div className="flex flex-col">
+        {/* Featured Articles */}
+        <div className="flex gap-2 justify-center" >
+          <Image
+           src="/images/Ken-Ijima-Header.jpeg"
+           height={100}
+           width={100}
+           alt="Picture of Ken Ijima, founder of Vujade"
+          />
 
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
+        </div>
+        <div className="flex justify-center">
             <a
               className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -19,7 +27,6 @@ export default function Home() {
               Discover more →
             </a>
           </div>
-        </main>
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
